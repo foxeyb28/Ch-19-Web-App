1e10171b0f7de24c2dfd033192d589b35a286e30
foxeyb28 <131126615+foxeyb28@users.noreply.github.com>
2023-10-01 18:37:03 -0500

    added code to js files

--- Develop/client/src/js/database.js
+++ Develop/client/src/js/database.js
@@ -1,21 +1,39 @@
-import { openDB } from 'idb';
+import { openDB } from "idb";
+import editor from "./editor";
 
 const initdb = async () =>
-  openDB('jate', 1, {
+  openDB("editor", 1, {
     upgrade(db) {
-      if (db.objectStoreNames.contains('jate')) {
-        console.log('jate database already exists');
+      if (db.objectStoreNames.contains("editor")) {
+        console.log("editor database already exists");
         return;
       }
-      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
-      console.log('jate database created');
+      db.createObjectStore("editor", { keyPath: "id", autoIncrement: true });
+      console.log("editor database created");
     },
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  console.log("putDb not implemented");
+  const editorDb = await openDB("editor", 1);
+  const tx = editorDb.transaction("editor", "readwrite");
+  const store = tx.objectStore("editor");
+  const request = store.put({ id: id, editor: content });
+  const result = await request;
+  console.log("data saved to the database", result);
+};
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  console.log("getDb not implemented");
+  const editorDb = await openDB(editor, 1);
+  const tx = editorDb.transaction("editor", "readonly");
+  const store = tx.objectStore("editor");
+  const request = store.getAll();
+  const result = await request;
+  console.log("result.value", result);
+  return result;
+};
 
 initdb();
