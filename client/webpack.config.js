84c297e3264143d0f5cb52e6c4b8d516915f1408
foxeyb28 <131126615+foxeyb28@users.noreply.github.com>
2023-10-01 21:18:43 -0500

    corrected syntax err in webpack

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,47 +1,47 @@
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const WebpackPwaManifest = require("webpack-pwa-manifest");
-const path = require("path");
-const { InjectManifest } = require("workbox-webpack-plugin");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const WebpackPwaManifest = require('webpack-pwa-manifest');
+const path = require('path');
+const { InjectManifest } = require('workbox-webpack-plugin');
 
 // TODO: Add and configure workbox plugins for a service worker and manifest file.
 // TODO: Add CSS loaders and babel to webpack.
 
 module.exports = () => {
   return {
-    mode: "development",
+    mode: 'development',
     entry: {
-      main: "./src/js/index.js",
-      install: "./src/js/install.js",
-      editor: "./src/js/editor.js",
+      main: './src/js/index.js',
+      install: './src/js/install.js',
+      editor: './src/js/editor.js',
     },
     output: {
-      filename: "[name].bundle.js",
-      path: path.resolve(__dirname, "dist"),
+      filename: '[name].bundle.js',
+      path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
       new HtmlWebpackPlugin({
-        template: "./index.html",
-        title: "Text Editor",
+        template: './index.html',
+        title: 'Text Editor',
       }),
       new InjectManifest({
-        swSrc: "./src-sw.js",
-        swDest: "src-sw.js",
+        swSrc: './src-sw.js',
+        swDest: 'src-sw.js',
       }),
       new WebpackPwaManifest({
         fingerprints: false,
         inject: true,
-        name: "Tex Editor",
-        short_name: "Editor",
-        description: "Just Another Text Editor!",
-        background_color: "#225ca3",
-        theme_color: "#225ca3",
-        start_url: "./",
-        publicPath: "./",
+        name: 'Tex Editor',
+        short_name: 'Editor',
+        description: 'Just Another Text Editor!',
+        background_color: '#225ca3',
+        theme_color: '#225ca3',
+        start_url: './',
+        publicPath: './',
         icons: [
           {
-            src: path.resolve("src/images/logo.png"),
+            src: path.resolve('src/images/logo.png'),
             sizes: [96, 128, 192, 256, 384, 512],
-            destination: path.join("assets", "icons"),
+            destination: path.join('assets", "icons'),
           },
         ],
       }),
@@ -51,19 +51,19 @@ module.exports = () => {
       rules: [
         {
           test: /\.css$/i,
-          use: [MiniCssExtractPlugin.loader, "css-loader"],
+          use: [MiniCssExtractPlugin.loader, 'css-loader'],
         },
         {
           test: /\.m?js$/,
           exclude: /node_modules/,
 
           use: {
-            loader: "babel-loader",
+            loader: 'babel-loader',
             options: {
-              presets: ["@babel/preset-env"],
+              presets: ['@babel/preset-env'],
               plugins: [
-                "@babel/plugin-proposal-object-rest-spread",
-                "@babel/transform-runtime",
+                '@babel/plugin-proposal-object-rest-spread',
+                '@babel/transform-runtime',
               ],
             },
           },
