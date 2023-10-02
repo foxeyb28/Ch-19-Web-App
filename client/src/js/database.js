798b5e1b24c60e4c31ccbae5c65f82063a1793b4
foxeyb28 <131126615+foxeyb28@users.noreply.github.com>
2023-10-02 18:24:56 -0500

    pushing to heroku

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -14,7 +14,7 @@ const initdb = async () =>
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
-  console.log('putDb not implemented');
+  //console.log('putDb not implemented');
   const jateDb = await openDB('jate', 1);
   const tx = jateDb.transaction('jate', 'readwrite');
   const store = tx.objectStore('jate');
@@ -25,8 +25,8 @@ export const putDb = async (content) => {
 
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => {
-  console.log('getDb not implemented');
-  const jateDb = await openDB(jate, 1);
+  //console.log('getDb not implemented');
+  const jateDb = await openDB('jate', 1);
   const tx = jateDb.transaction('jate', 'readonly');
   const store = tx.objectStore('jate');
   const request = store.getAll();
