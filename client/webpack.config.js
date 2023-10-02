d90096766c9dcd359755f8a4ea398cf010353e47
foxeyb28 <131126615+foxeyb28@users.noreply.github.com>
2023-10-02 18:13:19 -0500

    pushing to heroku

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,6 +1,6 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const WebpackPwaManifest = require('webpack-pwa-manifest');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+//const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
@@ -13,7 +13,7 @@ module.exports = () => {
     entry: {
       main: './src/js/index.js',
       install: './src/js/install.js',
-      //editor: './src/js/editor.js',
+      
     },
     output: {
       filename: '[name].bundle.js',
@@ -24,7 +24,7 @@ module.exports = () => {
         template: './index.html',
         title: 'Text Editor',
       }),
-      new MiniCssExtractPlugin(),
+      //new MiniCssExtractPlugin(),
       new InjectManifest({
         swSrc: './src-sw.js',
         swDest: 'src-sw.js',
